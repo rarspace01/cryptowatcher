95179bb6acc424090c4b3b803f29a2d3caa09964
Denis Hamann <hamannde@media-saturn.com>
2022-02-14 09:09:16 +0100

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,77 @@
+plugins {
+    val kotlinVersion = "1.6.10"
+    id("org.jetbrains.kotlin.jvm") version kotlinVersion
+    id("org.jetbrains.kotlin.plugin.allopen") version kotlinVersion
+
+    id("io.quarkus")
+}
+
+group = "com.cryptowatcher"
+version = "1.0.0"
+
+object Versions {
+    const val kotlinxCoroutinesVersion = "1.6.0"
+}
+
+repositories {
+    mavenCentral()
+    mavenLocal()
+    maven {
+        url = uri("https://oss.sonatype.org/content/repositories/snapshots")
+    }
+}
+
+val quarkusPlatformGroupId: String by project
+val quarkusPlatformArtifactId: String by project
+val quarkusPlatformVersion: String by project
+
+dependencies {
+    implementation(enforcedPlatform("$quarkusPlatformGroupId:$quarkusPlatformArtifactId:$quarkusPlatformVersion"))
+    implementation("io.quarkus:quarkus-resteasy-jackson")
+    implementation("io.quarkus:quarkus-kotlin")
+    implementation("io.quarkus:quarkus-resteasy")
+    implementation("io.quarkus:quarkus-scheduler")
+    implementation("io.quarkus:quarkus-arc")
+
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation(enforcedPlatform("org.jetbrains.kotlinx:kotlinx-coroutines-bom:${Versions.kotlinxCoroutinesVersion}"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-jvm")
+
+    testImplementation("io.quarkus:quarkus-junit5")
+    testImplementation("io.rest-assured:rest-assured")
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
+allOpen {
+    annotation("javax.ws.rs.Path")
+    annotation("javax.enterprise.context.ApplicationScoped")
+    annotation("io.quarkus.test.junit.QuarkusTest")
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+    kotlinOptions {
+        this.jvmTarget = JavaVersion.VERSION_1_8.toString()
+        this.freeCompilerArgs += "-Xopt-in=kotlin.RequiresOptIn"
+        this.javaParameters = true
+    }
+}
+
+tasks.named<Test>("test") {
+    useJUnitPlatform()
+    filter {
+        isFailOnNoMatchingTests = false
+    }
+    testLogging {
+        showExceptions = true
+        showStandardStreams = true
+        events = setOf(
+            org.gradle.api.tasks.testing.logging.TestLogEvent.FAILED,
+            org.gradle.api.tasks.testing.logging.TestLogEvent.PASSED
+        )
+        exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
+    }
+}
---
+++ src/main/kotlin/rarspace01/scheduler/TickerWatcher.kt
@@ -0,0 +1,63 @@
+package rarspace01.scheduler
+
+import com.fasterxml.jackson.core.JsonProcessingException
+import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.ObjectMapper
+import io.quarkus.scheduler.Scheduled
+import rarspace01.notification.TelegramService
+import rarspace01.utilities.HttpHelper
+import java.util.concurrent.atomic.AtomicInteger
+import java.util.function.Consumer
+import javax.enterprise.context.ApplicationScoped
+import javax.inject.Inject
+
+@ApplicationScoped
+class TickerWatcher {
+    private val counter = AtomicInteger()
+    fun get(): Int {
+        return counter.get()
+    }
+
+    @Inject
+    lateinit var telegramService: TelegramService
+
+    @Scheduled(every = "10s")
+    fun increment() {
+        println("get nomics")
+        val apiKey = "13853957f76b11ca1d0b0145b0e3e0493b23941b"
+        val page = HttpHelper().getPage(
+            "https://api.nomics.com/v1/currencies/ticker?key=$apiKey&ids=EWT3&interval=1d&convert=EUR&per-page=100&page=1"
+        )
+        val objectMapper = ObjectMapper()
+        try {
+            val jsonNodeRoot = objectMapper.readTree(page)
+            jsonNodeRoot.forEach(
+                Consumer { jsonNode: JsonNode ->
+                    val symbolId = jsonNode["id"].asText()
+                    val tickerPrice = jsonNode["price"].asDouble()
+                    if(symbolId == "EWT3" && tickerPrice<0.3) {
+                        telegramService.sendMessage("ETW3 below 0.5€")
+                    }
+                }
+            )
+        } catch (e: JsonProcessingException) {
+            e.printStackTrace()
+        }
+        // convert to object
+        // get price
+        // if price < limit alert
+        // alert subscriber of token
+    }
+
+//    @Scheduled(every = "10s")
+//    fun takleTGMessages() {
+//        println("get TGM")
+//        telegramService!!.getNewMessages().forEach(
+//            Consumer { (chatId, text): Message ->
+//                telegramService!!.sendMessage(
+//                    chatId, text
+//                )
+//            }
+//        )
+//    }
+}
