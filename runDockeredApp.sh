d8820f2467846595f95cdfa893c9313f192baa93
Denis Hamann <hamannde@media-saturn.com>
2022-02-14 19:36:07 +0100

    fix docker pull

--- runDockeredApp.sh
+++ runDockeredApp.sh
@@ -1,3 +1,3 @@
 echo $CR_PAT | docker login ghcr.io -u rarspace01 --password-stdin
-docker pull rarspace01/cryptowatcher
-docker run --restart always -d -it -p 8080:8080 rarspace01/cryptowatcher
+docker pull ghcr.io/rarspace01/cryptowatcher
+docker run --restart always -d -it -p 8080:8080 ghcr.io/rarspace01/cryptowatcher
