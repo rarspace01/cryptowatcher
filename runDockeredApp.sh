4e1c6dd8f83f758518dad19f2acc9734c43528ac
rarspace01 <rarspace01@gmail.com>
2022-02-16 18:20:54 +0100

    remove open ports

--- runDockeredApp.sh
+++ runDockeredApp.sh
@@ -1,2 +1,2 @@
 echo $CR_PAT | docker login ghcr.io -u rarspace01 --password-stdin
-docker run --env-file .env --restart always -d -it -p 8080:8080 ghcr.io/rarspace01/cryptowatcher
+docker run --env-file .env --restart always -d -it ghcr.io/rarspace01/cryptowatcher
