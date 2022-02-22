e99223839019365698dc57686e1297f99c8e3483
rarspace01 <rarspace01@users.noreply.github.com>
2022-02-22 20:16:57 +0100

    add new search functions

--- src/main/kotlin/rarspace01/notification/SubscriptionRepository.kt
+++ src/main/kotlin/rarspace01/notification/SubscriptionRepository.kt
@@ -5,6 +5,7 @@ import javax.enterprise.context.ApplicationScoped
 
 @ApplicationScoped
 class SubscriptionRepository : PanacheMongoRepository<Subscription> {
-    fun findByTickerAndUser(ticker: String, user: String) = find("ticker", ticker).firstResult<Subscription>()
+    fun findByTickerAndUser(ticker: String, user: String) = find(" {\"ticker\": \"$ticker\", \"user\": \"$user\"}").firstResult<Subscription>()
     fun findByUser(user: String) = find("user", user).stream<Subscription>()
+    fun findByTicker(ticker: String) = find("ticker", ticker).stream<Subscription>()
 }
