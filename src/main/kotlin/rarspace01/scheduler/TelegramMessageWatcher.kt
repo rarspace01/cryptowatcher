5ec5aefb6274075a0dbdb128da77de5e0041d6da
rarspace01 <rarspace01@users.noreply.github.com>
2022-02-22 20:09:54 +0100

    improve TG Message handler

--- src/main/kotlin/rarspace01/scheduler/TelegramMessageWatcher.kt
+++ src/main/kotlin/rarspace01/scheduler/TelegramMessageWatcher.kt
@@ -9,6 +9,8 @@ import javax.inject.Inject
 @ApplicationScoped
 class TelegramMessageWatcher {
 
+    val subscriptionRegex = Regex("([A-Za-z0-9]+)\\s([<>]+)\\s([0-9.,]+)")
+
     @Inject
     lateinit var telegramService: TelegramService
 
@@ -18,23 +20,33 @@ class TelegramMessageWatcher {
     @Scheduled(every = "5s")
     fun checkForNewMessages() {
         println("get TG messages")
-        telegramService.getNewMessages().forEach {
-            if (it.message == "/start") {
-                telegramService.sendMessage(it.chatId, "you are now subscribed! Add token to watch with `/add TokenId`  you can stop anytime with `/stop`")
-            } else if (it.message.startsWith("/add")) {
-                tickerSubscriptionService.addSubscription(it.message.replace("/add ", ""), it.chatId)
-            } else if (it.message.startsWith("/stop")) {
-                tickerSubscriptionService.removeAllSubscriptions(it.chatId)
-            } else if (it.message.startsWith("/help")) {
+        telegramService.getNewMessages().forEach { message ->
+            if (message.message == "/start") {
+                telegramService.sendMessage(message.chatId, "you are now subscribed! Add token to watch with `/add TokenId`  you can stop anytime with `/stop`")
+            } else if (message.message.startsWith("/add")) {
+                val messageFromUser = message.message.replace("/add ", "")
+                subscriptionRegex.findAll(messageFromUser).iterator().forEach {
+                    if (it.groups.size == 3) {
+                        val ticker = it.groups[1]?.value ?: ""
+                        val isLessThan = it.groups[2]?.value == "<"
+                        val tickerValue = it.groups[3]?.value?.toDoubleOrNull() ?: -1.0
+                        tickerSubscriptionService.addSubscription(ticker, message.chatId, tickerValue, isLessThan)
+                    } else {
+                        telegramService.sendMessage(message.chatId, "please use correct format. check /help")
+                    }
+                }
+            } else if (message.message.startsWith("/stop")) {
+                tickerSubscriptionService.removeAllSubscriptions(message.chatId)
+            } else if (message.message.startsWith("/help")) {
                 telegramService.sendMessage(
-                    it.chatId,
+                    message.chatId,
                     "`/stop` stops all subscriptions\n" +
                         "`/add BTC < 0.5` - subscribe to ticker when below 0.50€\n" +
                         "`/stop BTC` - remove subscriptions to ticker"
                 )
             } else {
-                println(it.message + "@" + it.chatId)
-                telegramService.sendMessage(it.chatId, "message not yet supported. use /help for commands")
+                println(message.message + "@" + message.chatId)
+                telegramService.sendMessage(message.chatId, "message not yet supported. use /help for commands")
             }
         }
     }
