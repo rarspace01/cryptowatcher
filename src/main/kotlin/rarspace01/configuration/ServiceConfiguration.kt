48c8fe97934ff8504b6f1c6fc2211df9009ddcff
rarspace01 <rarspace01@users.noreply.github.com>
2022-02-26 10:35:50 +0100

    add annotations

--- src/main/kotlin/rarspace01/configuration/ServiceConfiguration.kt
+++ src/main/kotlin/rarspace01/configuration/ServiceConfiguration.kt
@@ -2,7 +2,12 @@ package rarspace01.configuration
 
 import io.quarkus.mongodb.panache.PanacheMongoEntity
 import io.quarkus.mongodb.panache.common.MongoEntity
+import org.bson.codecs.pojo.annotations.BsonCreator
+import org.bson.codecs.pojo.annotations.BsonProperty
 import org.bson.types.ObjectId
 
 @MongoEntity
-data class ServiceConfiguration(val id: ObjectId? = null, val offset: Long) : PanacheMongoEntity()
+data class ServiceConfiguration @BsonCreator constructor(
+    @BsonProperty val id: ObjectId? = null,
+    @BsonProperty val offset: Long = 0L
+) : PanacheMongoEntity()
--- src/main/kotlin/rarspace01/notification/Subscription.kt
+++ src/main/kotlin/rarspace01/notification/Subscription.kt
@@ -2,7 +2,15 @@ package rarspace01.notification
 
 import io.quarkus.mongodb.panache.PanacheMongoEntity
 import io.quarkus.mongodb.panache.common.MongoEntity
+import org.bson.codecs.pojo.annotations.BsonCreator
+import org.bson.codecs.pojo.annotations.BsonProperty
 import org.bson.types.ObjectId
 
 @MongoEntity
-data class Subscription(val id: ObjectId? = null, val ticker: String, val user: String, val value: Double, val isLessThan: Boolean) : PanacheMongoEntity()
+data class Subscription @BsonCreator constructor(
+    @BsonProperty val id: ObjectId? = null,
+    @BsonProperty val ticker: String,
+    @BsonProperty val user: String,
+    @BsonProperty val value: Double,
+    @BsonProperty val isLessThan: Boolean
+) : PanacheMongoEntity()
