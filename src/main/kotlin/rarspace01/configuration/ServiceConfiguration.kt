e10d63cc7738e6e078f0c3932c45c041b52bfd41
rarspace01 <rarspace01@users.noreply.github.com>
2022-02-26 10:23:07 +0100

    disable integration test, append MongoEntity with annotation

--- src/main/kotlin/rarspace01/configuration/ServiceConfiguration.kt
+++ src/main/kotlin/rarspace01/configuration/ServiceConfiguration.kt
@@ -1,5 +1,8 @@
 package rarspace01.configuration
 
+import io.quarkus.mongodb.panache.PanacheMongoEntity
+import io.quarkus.mongodb.panache.common.MongoEntity
 import org.bson.types.ObjectId
 
-data class ServiceConfiguration(val id: ObjectId? = null, val offset: Long)
+@MongoEntity
+data class ServiceConfiguration(val id: ObjectId? = null, val offset: Long) : PanacheMongoEntity()
--- src/main/kotlin/rarspace01/notification/Subscription.kt
+++ src/main/kotlin/rarspace01/notification/Subscription.kt
@@ -1,5 +1,8 @@
 package rarspace01.notification
 
+import io.quarkus.mongodb.panache.PanacheMongoEntity
+import io.quarkus.mongodb.panache.common.MongoEntity
 import org.bson.types.ObjectId
 
-data class Subscription(val id: ObjectId? = null, val ticker: String, val user: String, val value: Double, val isLessThan: Boolean)
+@MongoEntity
+data class Subscription(val id: ObjectId? = null, val ticker: String, val user: String, val value: Double, val isLessThan: Boolean) : PanacheMongoEntity()
