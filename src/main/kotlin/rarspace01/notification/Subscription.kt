49102ac184e31390d7fd846f780a52952df1be35
rarspace01 <rarspace01@users.noreply.github.com>
2022-02-26 11:50:10 +0100

    remove noise

--- src/main/kotlin/rarspace01/notification/Subscription.kt
+++ src/main/kotlin/rarspace01/notification/Subscription.kt
@@ -1,16 +1,11 @@
 package rarspace01.notification
 
-import io.quarkus.mongodb.panache.PanacheMongoEntity
-import io.quarkus.mongodb.panache.common.MongoEntity
-import org.bson.codecs.pojo.annotations.BsonCreator
-import org.bson.codecs.pojo.annotations.BsonProperty
 import org.bson.types.ObjectId
 
-@MongoEntity
-data class Subscription @BsonCreator constructor(
-    @BsonProperty val id: ObjectId? = null,
-    @BsonProperty val ticker: String,
-    @BsonProperty val user: String,
-    @BsonProperty val value: Double,
-    @BsonProperty val isLessThan: Boolean
-) : PanacheMongoEntity()
+data class Subscription(
+    val id: ObjectId? = null,
+    val ticker: String = "",
+    val user: String = "",
+    val value: Double = 0.0,
+    val isLessThan: Boolean = true,
+)
